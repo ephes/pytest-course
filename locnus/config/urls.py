1a9b19d97882397f485e6080ff31b27f2d46be92
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-05-06 15:30:29 +0200

    redirect root page to client list

--- locnus/config/urls.py
+++ locnus/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="/client-list/"), name="home"),
     path("", include("client.urls", namespace="client")),
 ]
