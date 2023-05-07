4c278ed6395eacf242733c2bf4a5c85b312d6ac6
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-05-07 17:37:28 +0200

    no longer needed

--- tests/day1/first.py
+++
@@ -1,6 +0,0 @@
-def add_numbers(a, b):
-    return a + b
-
-
-def concat_strings(a, b):
-    return a + b
--- tests/day1/first_test.py
+++
@@ -1,9 +0,0 @@
-from .first import add_numbers
-
-
-def test_passing():
-    assert (1, 2, 3) == (1, 2, 3)
-
-
-def test_add_numbers():
-    assert add_numbers(1, 2) == 3
